3ce8895814b1e0b19ecf403faff58d06b3bca1ba
John-Theo <36699957+John-Theo@users.noreply.github.com>
2018-04-06 21:41:03 +0800

    Upload a broken version
    juj

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,17 @@
 var app = getApp();
+var util = require('../../utils/util-d.js'); 
 Page({
   data: {
     winHeight: "",//窗口高度
     currentTab: 0, //预设当前项的值
     scrollLeft: 0, //tab标题的滚动条位置
-    date: '2016-09-01',
+    date: '2018-04-01',
     delete_item: false,
-    expertList: [{ //假数据
-      img: "avatar.png",
-      name: "欢顔",
-      tag: "知名情感博主",
-      answer: 134,
-      listen: 2234
-    }]
+    itemIdList: [],
+    isempty: true,
+    proj_name: '',
+    proj_code: '',
+    openid: '',
   },
   // 滚动切换标签样式
   switchTab: function (e) {
@@ -50,13 +49,85 @@ Page({
       date: e.detail.value
     })
   },
-  create_new:function(e){
-
+  creatProj: function(e){
+    var that=this;
+    var aid = this.data.openid;
+    console.log(aid);
+    wx.request({
+      url: 'https://mytime.houaa.xyz/new/',
+      method: 'GET',
+      data: {
+        ownerid: aid,
+        name: that.data.proj_name,
+        date: that.data.date,
+      },
+      success: function (res) {
+        wx.showToast({
+          title: '创建成功',
+        })
+        wx.navigateTo({
+          url: '../calendar/calendar?itemid='+res.data,
+        })
+        console.log(res.data)
+      }
+    })
   },
-  delete_item:function(){
-    this.setData({delete_item:true})
+  detail: function(e) {
+    // console.log(e.currentTarget.id);
+    wx.navigateTo({
+      url: "../calendar/calendar?itemid=" + e.currentTarget.id,
+    })
+  },
+  mytextInput: function (e) {
+    this.setData({
+      proj_name: e.detail.value,
+      isempty: false,
+    })
+  },
+  mycodeInput: function (e) {
+    this.setData({
+      proj_code: e.detail.value,
+      isempty: false,
+    })
+  },
+  delete_item:function(e){
+    var that = this;
+    var list = this.data.itemIdList
+    var pname, index;
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].hex == e.currentTarget.id){
+        pname = list[i].name;
+        index = i;
+        break;
+      }
+    }
+    wx.showModal({
+      content: '真的要删除“' + pname + '”吗？',
+      cancelText: '点错打扰',
+      success: function (res) {
+        if (res.confirm) {
+          // console.log(e.currentTarget.id)
+          list.splice(index, 1);
+          that.setData({
+            itemIdList: list
+          });
+          var ipid = e.currentTarget.id;
+          wx.request({
+            url: 'https://mytime.houaa.xyz/delete/?pid=' + ipid,
+            method: 'GET',
+          })
+        }
+      }
+    })
   },
   onLoad: function (options) {
+    // 调用函数时，传入new Date()参数，返回值是日期和时间  
+    var time = util.formatTime(new Date());
+    // 再通过setData更改Page()里面的data，动态更新页面的数据  
+    this.setData({
+      date: time
+    });
+    
     if (options.currentTab){
       this.setData({
         currentTab: options.currentTab
@@ -73,6 +144,32 @@ Page({
         that.setData({
           winHeight: calc
         });
+      }
+    });
+    wx.login({
+      //获取code
+      success: function (res) {
+        var code = res.code; //返回code
+        wx.request({
+          url: 'https://mytime.houaa.xyz/wxcode/?code=' + code,
+          method: 'GET',
+          success: function (res) {
+            // that.data.openid = res.data;
+            // console.log(res.data);
+            that.setData({
+              openid: res.data
+            });
+            wx.request({
+              url: 'https://mytime.houaa.xyz/plist/?openid=' + res.data,
+              method: 'GET',
+              success: function (res) {
+                that.setData({
+                  itemIdList: res.data
+                });
+              }
+            })
+          }
+        })
       }
     });
   },
--- pages/result/result.js
+++ pages/result/result.js
@@ -1,25 +1,151 @@
-//result.js
+//create_new.js
 Page({
   data: {
-    currentTab: 0, //预设当前项的值
-    scrollLeft: 0, //tab标题的滚动条位置
-    share:true,
-    winWidth: '',
-    winHeight: "",
+    openid: '',
+    for_row: [],
+    for_daytime: [
+      { id: 1, time: 'm' },
+      { id: 2, time: 'l' },
+      { id: 3, time: 'a' },
+      { id: 4, time: 'd' },
+      { id: 5, time: 'e' },
+    ],
+    select: {},
+    total: {},
+    projectId: 'default',
+    ischosen: 'hidden',
+    max_time: 1,
+    tar_date: '等你长按确定时间哦！',
+    ownerid: '',
+
   },
-  onLoad: function () {
-    var that = this;
-    //  高度自适应
-    wx.getSystemInfo({
+  submita: function (e) {
+    var ipid = this.data.projectId;
+    var ioid = this.data.openid;
+    wx.request({
+      url: 'https://mytime.houaa.xyz/submit/?pid=' + ipid + '&openid=' + ioid,
+      method: 'GET',
+      data: this.data.select,
       success: function (res) {
-        var clientHeight = res.windowHeight,
-          clientWidth = res.windowWidth,
-          rpxR = 750 / clientWidth;
-        var calc = clientHeight * rpxR;
-        that.setData({
-          winHeight: calc
+        console.log(res.data)
+        wx.showToast({
+          title: '已提交！',
+        })
+      }
+    })
+  },
+  result: function (e) { wx.navigateTo({ url: "../result/result?pid=0" }) },
+  select_slot: function (e) {
+    var info = e.target.id;
+    var i = info[0];
+    var j = info[1];
+    var for_row = this.data.for_row;
+    var time = ['上午', '中饭', '下午', '晚饭', '晚上'];
+    if (this.data.openid != this.data.owner_id) {
+      wx.showModal({
+        content: '请问卷的发起者来确定吧！',
+      })
+    }
+    // console.log(this.data.total);
+    else {
+      this.setData({
+        tar_date: '约 ' + for_row[j].date + '号（星期' + for_row[j].day + '）的' + time[i - 1] + ' ！',
+        ischosen: 'visible',
+      });
+      wx.setClipboardData({
+        data: '我们就 ' + this.data.tar_date + '投票结果戳链接查看！',
+        success: function (res) {
+          wx.getClipboardData({
+            success: function (res) {
+              console.log(res.data) // data
+            }
+          })
+        }
+      })
+    }
+
+  },
+
+  onShareAppMessage: function (res) {
+
+    console.log(this.name);
+    if (res.from == 'button') {
+      // console.log(res.target.id);
+    }
+    return {
+      title: this.data.tar_date,
+      path: '/pages/result/result?pid=' + this.data.projectId,
+      success: function (res) {
+        console.log(tmp);
+      },
+    }
+  },
+
+  toNew: function (e) { wx.navigateTo({ url: "../index/index?currentTab=0" }) },
+  toHis: function (e) { wx.navigateTo({ url: "../index/index?currentTab=1" }) },
+
+  onLoad: function (options) {
+    var that = this;
+    this.setData({
+      projectId: options.pid,
+    });
+    // console.log(this.data.select);
+    // console.log(this.data.projectId);
+    wx.showToast({
+      title: '获取数据...',
+      icon: 'loading',
+      success: function (e) {
+        wx.login({
+          //获取code
+          success: function (res) {
+            var code = res.code; //返回code
+            wx.request({
+              url: 'https://mytime.houaa.xyz/wxcode/?code=' + code,
+              method: 'GET',
+              success: function (res) {
+                var iopenid = res.data;
+                that.setData({
+                  openid: iopenid
+                });
+                var ipid = that.data.projectId;
+                var ioid = that.data.openid;
+                wx.request({
+                  url: 'https://mytime.houaa.xyz/owner/?pid=' + ipid,
+                  method: 'GET',
+                  success: function (res) {
+                    that.setData({ owner_id: res.data })
+                    // console.log(that.data.owner_id)
+                  }
+                });
+                wx.request({
+                  url: 'https://mytime.houaa.xyz/total/?pid=' + ipid,
+                  method: 'GET',
+                  success: function (res) {
+                    if (res.data == 'NotExist') { console.log('Nobody filled...') }
+                    else {
+                      that.setData({ total: res.data.total, for_row: res.data.other })
+                      console.log(res.data)
+                    }
+                    var max = 0;
+                    var list = that.data.total;
+                    var dayl = ['e', 'd', 'a', 'l', 'm'];
+                    for (var i = 0; i < 7; i++) {
+                      for (var j = 0; j < 5; j++) {
+                        if (list[i][dayl[j]] > max) {
+                          max = list[i][dayl[j]];
+                        }
+                      }
+                    }
+                    // console.log(max);
+                    that.setData({ max_time: max })
+                    wx.hideToast();
+                  }
+                })
+              }
+            })
+          }
         });
       }
-    });
-  }
+    })
+  },
 })
